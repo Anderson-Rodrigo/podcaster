313346f5fc851fae217e0fbb89b2d1fc9eb2a224
Anderson-Rodrigo <anderson.vargas90@gmail.com>
2021-04-21 23:43:17 -0300

    aa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,48 @@
-import { Header } from "../components/Header";
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+type HomeProps = {  
+  episodes: Array
+}
+//SSG gera a pagina de forma estatica
+export default function Home(props){
+  console.log(props)
   return (
-    <div></div>
+    <div>
+      <h1>Index</h1>
+    </div>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+//exemplo de SSR controlado pelo next, para o front nao ficar toda vez fazer requisição
+//sempre sera retornado essa info
+// export default function Home(props){
+//   console.log(props)
+//   return (
+//     <div>
+//       <h1>Index</h1>
+//     </div>
+//   )
+// }
+
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
